4dc0ebdb7a59e7f1ed9af39583bd12083df0b4f9
marcosgs <mvinicius17112002@gmail.com>
2022-12-02 11:55:03 -0300

    ATL

---
+++ LISTA 5/EX 1 LISTA 5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+    int main (){
+        char nome[50];
+
+        printf("Digite o nome completo: ");
+        gets(nome);
+        printf("%d",strlen(nome));
+
+    }
